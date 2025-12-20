125c95863f5afc3c391bd0f904f5fbf420d975ca
anticrash <anticrash@users.noreply.github.com>
2025-12-19 22:12:57 -0800

    Update CdnImage.tsx

--- src/components/CdnImage.tsx
+++ src/components/CdnImage.tsx
@@ -6,7 +6,7 @@
 import React from 'react';
 import { getImageUrl } from '../lib/imageUtils';
 
-interface CdnImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+interface CdnImageProps extends Omit<React.ImgHTMLAttributes<HTMLImageElement>, 'src'> {
   src: string | null | undefined;
 }
 
